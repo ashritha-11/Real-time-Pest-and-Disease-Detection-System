12dfc9a1194a9593472f4ecb55a95deff31fc7ce
ashritha-11 <ashritha824@gmail.com>
2025-10-03 15:47:48 +0530

    Update supabase_client.py

--- src/dao/supabase_client.py
+++ src/dao/supabase_client.py
@@ -1,7 +1,11 @@
-from supabase import create_client
+import streamlit as st
+from supabase import create_client, Client
 
+# Use Streamlit secrets
 SUPABASE_URL = st.secrets["SUPABASE_URL"]
 SUPABASE_KEY = st.secrets["SUPABASE_KEY"]
+
+supabase: Client | None = None
 
 try:
     supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
