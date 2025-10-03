595a3f7022950af4075779a950d1b8ba3e1f8e0a
ashritha-11 <ashritha824@gmail.com>
2025-10-03 15:01:55 +0530

    Update supabase_client.py

--- src/dao/supabase_client.py
+++ src/dao/supabase_client.py
@@ -1,4 +1,4 @@
-from supabase import Client, create_client
+from supabase import create_client, Client
 import os
 from dotenv import load_dotenv
 
@@ -6,12 +6,7 @@ load_dotenv()
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
 
-supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+if not SUPABASE_URL or not SUPABASE_KEY:
+    raise Exception("Supabase credentials not found. Set SUPABASE_URL and SUPABASE_KEY in .env")
 
-# Insert a record
-data = {
-    "user_id": 1,
-    "image_name": "temp.jpg",
-    "prediction": "Pest_Affected"
-}
-supabase.table("detections").insert(data).execute()  # Works in 0.x versions
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
